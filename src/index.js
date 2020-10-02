09cf2bc3426339a8e8714d6476e365d31186a000
Nastya <asekerich99@gmail.com>
2020-10-03 00:36:48 +0300

    feat: add functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
-
 exports.min = function min (array) {
-  return 0;
-}
-
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+  if (array === undefined || array.length == 0){
+    return 0;
+  } else {
+    const min = Math.min.apply(null, array);
+    return min;
+  };
+  }
+  
+  exports.max = function max (array) {
+    if (array === undefined || array.length == 0){
+    return 0;
+  } else {
+    const max = Math.max.apply(null, array);
+    return max;
+  };
+  }
+  
+  exports.avg = function avg (array) {
+    if (array === undefined || array.length == 0){
+    return 0;
+  } else {
+    return array.reduce(((a, b) => a + b))/array.length;
+  };
+  }
+  
